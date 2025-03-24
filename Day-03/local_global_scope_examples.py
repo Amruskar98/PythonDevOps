937b3e018a196639dec8c8c612b875e10ee6925c
Amruskar98 <115529437+Amruskar98@users.noreply.github.com>
2025-03-24 05:55:39 +0000

    local and global varible exmaple

---
+++ Day-03/local_global_scope_examples.py
@@ -0,0 +1,13 @@
+x = 50 # global variable
+y = 30
+def addition():
+    x = 10 # local variable
+    y = 40
+    print(x+y)
+def substraction():
+    #x = 50 # local variable
+    #y = 30
+    print(x-y)
+
+addition()
+substraction()  
