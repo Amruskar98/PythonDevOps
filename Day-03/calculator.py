08cf4251b1c9d1effe664fda41e9d4bf8e79904b
Amruskar98 <115529437+Amruskar98@users.noreply.github.com>
2025-03-24 06:18:26 +0000

    calulator program

---
+++ Day-03/calculator.py
@@ -0,0 +1,8 @@
+num1 = 100
+num2 = 200
+
+addition = num1 + num2
+print("Value of calculated = " + str(addition))
+
+substraction = num1 - num2
+print("value of substraction = " + str(substraction))
