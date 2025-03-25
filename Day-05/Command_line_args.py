92ae583c6b1674d23e139148097a8068cd9d32c2
Amruskar98 <115529437+Amruskar98@users.noreply.github.com>
2025-03-25 06:26:36 +0000

    command line argument code

---
+++ Day-05/Command_line_args.py
@@ -0,0 +1,25 @@
+import sys
+
+def add(num1, num2):
+    a = num1 + num2
+    return a
+
+def sub(num1, num2):
+    s = num1 - num2
+    return s
+
+def mul(num1, num2):
+    m = num1 * num2
+    return m    
+
+num1 = int(sys.argv[1])
+operation = sys.argv[2]
+num2 = int(sys.argv[3])
+
+if operation == "add":
+    output = add(num1, num2)
+    print(output)
+else:
+    operation == "mul"
+    output = mul(num1, num2)
+    print(output)
