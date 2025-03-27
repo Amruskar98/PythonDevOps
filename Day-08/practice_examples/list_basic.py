3aa2945781e8e815224aa7d04196dd064b415e16
Amruskar98 <115529437+Amruskar98@users.noreply.github.com>
2025-03-27 06:18:46 +0000

    listing exmples

---
+++ Day-08/practice_examples/list_basic.py
@@ -0,0 +1,11 @@
+list =["parsh","ramu","shamu","tina"]
+#list.remove("ramu")
+#print(list[3])
+new_list= list[1:4]
+#print (len(list))
+print(new_list)
+
+
+
+#list =("parsh","ramu","shamu","tina")
+#print(type(list))
